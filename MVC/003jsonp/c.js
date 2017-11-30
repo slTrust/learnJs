018bbf26170a421cd725e2525fd3ead70af160a4
huangjx <trustfor@sina.cn>
2017-12-01 00:46:17 +0800

    ADD

--- MVC/003jsonp/c.js
+++ MVC/003jsonp/c.js
@@ -1,5 +1,9 @@
 window.onload = function(){
     oT.onkeydown = function(){
-        
+        var val = oT.value;
+        data(val,function(res){
+            console.log(res)
+            show(ul1,res);
+        });  
     }
 }
