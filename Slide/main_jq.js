771c7319d40f4f877e1c835d09406ec8ec3b2866
huangjx <trustfor@sina.cn>
2017-12-22 10:31:13 +0800

    slider

---
+++ Slide/main_jq.js
@@ -0,0 +1,8 @@
+var aBtn = document.querySelectorAll('.buttonBox span');
+var len = aBtn.length;
+for(let i=0;i<len;i++){
+    aBtn[i].on('click',function(e){
+        let index = $(e.targetElement)
+        $('.content img').eq()
+    })
+}
