1cf495ee4067ee9f1fc260a5efe55c64a692a409
huangjx <trustfor@sina.cn>
2018-01-24 23:45:24 +0800

    原生实现ajax

---
+++ Ajax/ajax_demo/ajax.js
@@ -0,0 +1,53 @@
+function obj2str(obj){
+    //避免返回相同结果
+    obj.t = Math.random();
+    var arr = [];
+    for(var name in obj){
+        arr.push(name+'='+obj[name]);
+    }
+    var str = arr.join('&');
+    return str;
+}
+function ajax(paramObj){
+    let paramObj = paramObj||{};
+    paramObj.type = paramObj.type||'GET';
+    paramObj.url = paramObj.url||'';
+    if(paramObj.url === ''){ alert('请给我一个url'); return}
+    
+    let httpRequest = new XMLHttpRequest();
+
+    switch(paramObj.type.toUpperCase()){
+        case 'GET':
+            httpRequest.open('GET',paramObj.url+'?'+obj2str(paramObj.data),true);
+            httpRequest.send();
+            break;
+        case 'POST':
+            httpRequest.open('POST',paramObj.url,true);
+            httpRequest.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+            httpRequest.send(obj2str(paramObj.data));
+            break;
+    }
+
+    httpRequest.onreadystatechange = function(){
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status >= 200&&httpRequest.status<300||httpRequest.status === 304) {
+                paramObj.success&&paramObj.success(httpRequest.responseText);
+            } else {
+                paramObj.error&&paramObj.error(httpRequest.status);
+            }
+        }
+    }
+
+}
+//调用
+ajax({
+    type:"get",
+    url:'/xxx',
+    data:{user:"hjx"},
+    success:function(res){
+        console.log(res);
+    },
+    error:function(err){
+        console.log(res);
+    }
+})
